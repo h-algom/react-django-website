7c965365a0944dffdc7bf455f29b2489882bcccc
halgom <142266318+h-algom@users.noreply.github.com>
2023-11-04 11:52:36 +0000

    Update 4 files

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -1,5 +1,18 @@
 const Home = () => {
-    return <div>Home</div>
+    return <div className="main-wrapper">
+        <section className="hero-wrapper">
+            <div className="hero-container"></div>
+        </section>
+        <section className="features-wrapper">
+            <div className="features-container"></div>
+        </section>
+        <section className="usage-wrapper">
+            <div className="usage-container"></div>
+        </section>
+        <section className="testimony-wrapper">
+            <div className="testimony-container"></div>
+        </section>
+    </div>
 }
 
 export default Home;
